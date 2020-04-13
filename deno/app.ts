520fa21079c2666c6edcbcf4a4aa2eac06251c1e
Nitin Reddy <reddynitin3@gmail.com>
2020-04-13 19:27:49 +0530

    Tried out a secure ts runtime example using deno

---
+++ deno/app.ts
@@ -0,0 +1,10 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+app.listen({ port: 8000 });
+console.log("App is listening at port 8000...");
